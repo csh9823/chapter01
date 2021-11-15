3578d29bafc5ce1fc95013535240948f56bc896b
최현진 <chj@DESKTOP-3BFLJR4>
2021-11-15 19:01:34 +0900

    과제 최종

---
+++ src/com/javaex/ex03/Ex08.java
@@ -0,0 +1,29 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex08 {
+		public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력해 주세요");
+		System.out.print("숫자: ");
+		
+		int point = sc.nextInt();
+				
+		if(point==0){//false일때만
+			System.out.println("0");
+		}
+		else if(point%2==0) {
+			System.out.println("짝수");
+		}
+		else {
+			System.out.println("홀수");
+		}
+		
+		sc.close();
+	}
+
+}
+
