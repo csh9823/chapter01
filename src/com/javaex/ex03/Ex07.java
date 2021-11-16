2f61630a07f4c81d3e5555ad2aa85900384995e2
y <y@DESKTOP-6H8GUR4>
2021-11-16 11:10:37 +0900

    과제 수정

--- src/com/javaex/ex03/Ex07.java
+++ src/com/javaex/ex03/Ex07.java
@@ -9,7 +9,7 @@ public class Ex07 {
 			System.out.print("점수를 입력하세요");
 		
 			int a = sc.nextInt();
-			
+			/*
 			if(a>=90) {
 				System.out.println("A등급");
 			}
@@ -22,6 +22,25 @@ public class Ex07 {
 				
 			}
 			else if (a>=60 && a<=69) {
+				System.out.println("D등급");	
+			}
+			else {
+				System.out.println("F등급");
+			}
+			*/
+			// 이렇게 써도됨.
+			if(a>=90) {
+				System.out.println("A등급");
+			}
+			
+			else if (a>=80) {
+				System.out.println("B등급");
+			}
+			else if (a>=70) {
+				System.out.println("C등급");
+				
+			}
+			else if (a>=60) {
 				System.out.println("D등급");	
 			}
 			else {
